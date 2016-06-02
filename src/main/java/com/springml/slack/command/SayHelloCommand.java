12fdda4bfecad16909e335b27227574fbd3784fe
Samuel Alexander <samuel@palmtreeinfotech.com>
2016-06-02 19:11:17 +0530

    say_hello resource added

--- src/main/java/com/springml/slack/command/SayHelloCommand.java
+++ src/main/java/com/springml/slack/command/SayHelloCommand.java
@@ -2,11 +2,14 @@ package com.springml.slack.command;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.springml.slack.command.model.Command;
 
 @RestController
 public class SayHelloCommand {
@@ -18,6 +21,13 @@ public class SayHelloCommand {
         System.out.println("code : " + code);
         System.out.println("state : " + state);
         return new ResponseEntity<String>("message", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/slack/say_hello", method = RequestMethod.POST)
+    public @ResponseBody ResponseEntity<String> sayHello(@ModelAttribute Command command) throws Exception {
+
+        System.out.println("command : " + command);
+        return new ResponseEntity<String>("Hi " + command.getText(), HttpStatus.OK);
     }
 
 }
