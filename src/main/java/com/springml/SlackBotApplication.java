b5735f09614cb0e3ba134f1fe964f003f74cfc9f
Samuel Alexander <samuel@palmtreeinfotech.com>
2016-06-06 16:55:33 +0530

    Formatting

--- src/main/java/com/springml/SlackBotApplication.java
+++ src/main/java/com/springml/SlackBotApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SlackBotApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SlackBotApplication.class, args);
 	}
